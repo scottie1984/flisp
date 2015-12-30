69aaae70f68aced28572c77f00238fe3e758933e
Stephen Scott <s.p.scott@kainos.com>
2015-12-30 11:18:29 +0000

    First attempt at function definitions, some tidy up left

--- test/flisp.spec.js
+++ test/flisp.spec.js
@@ -65,118 +65,136 @@ describe('littleLisp', function() {
   describe('interpret', function() {
     describe('lists', function() {
       it('should return empty list', function() {
-        expect(t.interpret(t.parse('()'))).toEqual([]);
+        expect(t.run('()')).toEqual([]);
       });
 
       it('should return list of strings', function() {
-        expect(t.interpret(t.parse('("hi" "mary" "rose")'))).toEqual(['hi', "mary", "rose"]);
+        expect(t.run('("hi" "mary" "rose")')).toEqual(['hi', "mary", "rose"]);
       });
 
       it('should return list of numbers', function() {
-        expect(t.interpret(t.parse('(1 2 3)'))).toEqual([1, 2, 3]);
+        expect(t.run('(1 2 3)')).toEqual([1, 2, 3]);
       });
 
       it('should return list of numbers in strings as strings', function() {
-        expect(t.interpret(t.parse('("1" "2" "3")'))).toEqual(["1", "2", "3"]);
+        expect(t.run('("1" "2" "3")')).toEqual(["1", "2", "3"]);
       });
     });
 
     describe('atoms', function() {
       it('should return string atom', function() {
-        expect(t.interpret(t.parse('"a"'))).toEqual("a");
+        expect(t.run('"a"')).toEqual("a");
       });
 
       it('should return string with space atom', function() {
-        expect(t.interpret(t.parse('"a b"'))).toEqual("a b");
+        expect(t.run('"a b"')).toEqual("a b");
       });
 
       it('should return string with opening paren', function() {
-        expect(t.interpret(t.parse('"(a"'))).toEqual("(a");
+        expect(t.run('"(a"')).toEqual("(a");
       });
 
       it('should return string with closing paren', function() {
-        expect(t.interpret(t.parse('")a"'))).toEqual(")a");
+        expect(t.run('")a"')).toEqual(")a");
       });
 
       it('should return string with parens', function() {
-        expect(t.interpret(t.parse('"(a)"'))).toEqual("(a)");
+        expect(t.run('"(a)"')).toEqual("(a)");
       });
 
       it('should return number atom', function() {
-        expect(t.interpret(t.parse('123'))).toEqual(123);
+        expect(t.run('123')).toEqual(123);
       });
     });
 
     describe('invocation', function() {
       it('should run print on an int', function() {
-        expect(t.interpret(t.parse("(print 1)"))).toEqual(1);
+        expect(t.run("(print 1)")).toEqual(1);
       });
 
       it('should return first element of list', function() {
-        expect(t.interpret(t.parse("(head (1 2 3))"))).toEqual(1);
+        expect(t.run("(head (1 2 3))")).toEqual(1);
       });
 
       it('should return rest of list', function() {
-        expect(t.interpret(t.parse("(tail (1 2 3))"))).toEqual([2, 3]);
+        expect(t.run("(tail (1 2 3))")).toEqual([2, 3]);
       });
     });
 
     describe('lambdas', function() {
       it('should return correct result when invoke lambda w no params', function() {
-        expect(t.interpret(t.parse("((lambda () (tail (1 2))))"))).toEqual([2]);
+        expect(t.run("((lambda () (tail (1 2))))")).toEqual([2]);
       });
 
       it('should return correct result for lambda that takes and returns arg', function() {
-        expect(t.interpret(t.parse("((lambda (x) x) 1)"))).toEqual(1);
+        expect(t.run("((lambda (x) x) 1)")).toEqual(1);
       });
 
       it('should return correct result for lambda that returns list of vars', function() {
-        expect(t.interpret(t.parse("((lambda (x y) (x y)) 1 2)"))).toEqual([1, 2]);
+        expect(t.run("((lambda (x y) (x y)) 1 2)")).toEqual([1, 2]);
       });
 
       it('should get correct result for lambda that returns list of lits + vars', function() {
-        expect(t.interpret(t.parse("((lambda (x y) (0 x y)) 1 2)"))).toEqual([0, 1, 2]);
+        expect(t.run("((lambda (x y) (0 x y)) 1 2)")).toEqual([0, 1, 2]);
       });
 
       it('should return correct result when invoke lambda w params', function() {
-        expect(t.interpret(t.parse("((lambda (x) (head (x))) 1)")))
+        expect(t.run("((lambda (x) (head (x))) 1)"))
           .toEqual(1);
       });
     });
 
+    describe('def', function() {
+	  it('should allow a function to be defined', function() {
+		expect(t.run("(def foo (x y) (add x y))")).toEqual('FOO');
+	  });
+
+	  it('should allow a function to be defined and called', function() {
+		expect(t.run("((def foo (x y) (add x y)) (foo 4 3))")).toEqual([ 'FOO', 7 ]);
+	  });
+
+	  it('should allow a function to be defined and called later in module', function() {
+		expect(t.run("((def foo (x y) (add x y)) (1) (2) (foo 4 3))")).toEqual([ 'FOO', [ 1 ], [ 2 ], 7 ]);
+	  });
+
+	  it('should return undefined when function is not defined', function() {
+		expect(t.run("((foo 4 3))")).toEqual([ [ undefined, 4, 3 ] ]);
+	  });
+	});
+
     describe('let', function() {
       it('should eval inner expression w names bound', function() {
-        expect(t.interpret(t.parse("(let ((x 1) (y 2)) (x y))"))).toEqual([1, 2]);
+        expect(t.run("(let ((x 1) (y 2)) (x y))")).toEqual([1, 2]);
       });
 
       it('should not expose parallel bindings to each other', function() {
         // Expecting undefined for y to be consistent with normal
         // identifier resolution in littleLisp.
-        expect(t.interpret(t.parse("(let ((x 1) (y x)) (x y))"))).toEqual([1, undefined]);
+        expect(t.run("(let ((x 1) (y x)) (x y))")).toEqual([1, undefined]);
       });
 
       it('should accept empty binding list', function() {
-        expect(t.interpret(t.parse("(let () 42)"))).toEqual(42);
+        expect(t.run("(let () 42)")).toEqual(42);
       });
     });
 
     describe('if', function() {
       it('should choose the right branch', function() {
-        expect(t.interpret(t.parse("(if 1 42 4711)"))).toEqual(42);
-        expect(t.interpret(t.parse("(if 0 42 4711)"))).toEqual(4711);
+        expect(t.run("(if 1 42 4711)")).toEqual(42);
+        expect(t.run("(if 0 42 4711)")).toEqual(4711);
       });
     });
 
     describe('add', function() {
 	  it('should add 2 numbers', function() {
-		expect(t.interpret(t.parse("(add 1.3 1.5)"))).toEqual(2.8);
-		expect(t.interpret(t.parse("(add 3 5)"))).toEqual(8);
+		expect(t.run("(add 1.3 1.5)")).toEqual(2.8);
+		expect(t.run("(add 3 5)")).toEqual(8);
 	  });
 	});
 
     describe('compose', function() {
 	  it('should compose correctly', function() {
-		expect(t.interpret(t.parse(`(
+		expect(t.run(`(
                                     let ((x (
                                     	(compose
                                     		(filter (lambda (x) (gt 5 x)))
@@ -185,7 +203,7 @@ describe('littleLisp', function() {
                                     	)
                                     	(1 2 3)
                                     )) (y 4)) (print x)
-                                    )`))).toEqual([3,4]);
+                                    )`)).toEqual([3,4]);
 	  });
 	});
   });
